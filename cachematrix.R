a1690a2c7714a4661188aebff0ba67544eda2259
Analesdey <analesin@AnalesDumiksMBP.fios-router.home>
2016-10-18 19:21:28 -0400

    Matrix Cached

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing functions to:
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the inverse
+##get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -20,7 +21,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the matrix 
+## using the previous function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
